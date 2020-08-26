17fd72941ec946d7d3360c722a6388164b9baa2f
Kim Sappi <ksappi@student.hive.fi>
2020-08-26 16:37:45 +0300

    4.2

--- part4/app.js
+++ part4/app.js
@@ -4,26 +4,13 @@ const mongoose = require('mongoose');
 
 const config = require('./utils/config');
 const app = express();
+const blogsRouter = require('./controllers/blogs');
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter);
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+module.exports = app;
--- part4/index.js
+++ part4/index.js
@@ -4,16 +4,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config');
 const app = require('./app');
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
 const server = http.createServer(app);
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
-})
+	console.log(`Server running on port ${config.PORT}`)
+});
