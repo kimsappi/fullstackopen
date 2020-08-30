ea01fe284a69b71c2b1c8ce3a0e4542d1b044f5d
Kim Sappi <ksappi@student.hive.fi>
2020-08-30 12:37:51 +0300

    5.7

--- part5/src/components/Blog.js
+++ part5/src/components/Blog.js
@@ -1,8 +1,32 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, {useState} from 'react'
+
+const BlogDetails = ({blog}) => (
+  <>
+  <div>{blog.url}</div>
+  <div>{blog.likes} <button>like</button></div>
+  <div>{blog.user.username}</div>
+  </>
+);
+
+const Blog = ({ blog }) => {
+  const [expanded, setExpanded] = useState(false);
+
+	const toggleExpansion = () => {
+		setExpanded(!expanded);
+	}
+
+  const blogStyle = {
+    border: '2px solid black',
+    margin: '0.5em',
+    padding: '0.5em'
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={toggleExpansion}>{expanded ? 'hide' : 'view'}</button>
+      {expanded && <BlogDetails blog={blog} />}
+    </div>
+  );
+};
 
 export default Blog
