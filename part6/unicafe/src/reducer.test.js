019cb8a402c9ef5b6769397c9bb13eab4cf15ae1
Kim Sappi <ksappi@student.hive.fi>
2020-08-31 14:58:14 +0300

    6.1

---
+++ part6/unicafe/src/reducer.test.js
@@ -0,0 +1,83 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+describe('unicafe reducer', () => {
+  const initialState = {
+    good: 0,
+    ok: 0,
+    bad: 0
+  }
+
+  test('should return a proper initial state when called with undefined state', () => {
+    const state = {}
+    const action = {
+      type: 'DO_NOTHING'
+    }
+
+    const newState = counterReducer(undefined, action)
+    expect(newState).toEqual(initialState)
+  })
+
+  test('good is incremented', () => {
+    const action = {
+      type: 'GOOD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 1,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('zero zeroes everything', () => {
+    const state = initialState
+
+    deepFreeze(state)
+    const state1 = counterReducer(state, {type: 'GOOD'})
+    const state2 = counterReducer(state1, {type: 'OK'})
+    const state3 = counterReducer(state2, {type: 'BAD'})
+    expect(state3).toEqual({
+      good: 1,
+      ok: 1,
+      bad: 1
+    })
+
+    deepFreeze(state3)
+    const finalState = counterReducer(state3, {type: 'ZERO'})
+    expect(finalState).toEqual(initialState)
+  })
+})
